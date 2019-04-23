8e36b21c0bb18aeef63e22d4200d184762f31e10
Majdziad <41499505+Majdziad@users.noreply.github.com>
2019-04-22 20:41:59 -0700

    Add files via upload

---
+++ commands/server.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+
+  let sicon = message.guild.iconURL;
+  let serverembed = new Discord.RichEmbed()
+  .setDescription("Server Information")
+  .setColor("#6a1515")
+  .setThumbnail(sicon)
+  .addField("Owner:", message.guild.owner)
+  .addField("Server Name:", message.guild.name)
+  .addField("Created On:", message.guild.createdAt)
+  .addField("You Joined:", message.member.joinedAt)
+  .addField("You Joined:", message.member.joinedAt)
+  .addField("Your Role:", message.guild.roles)
+  .addField("Total Members", message.guild.memberCount);
+  return message.channel.send(serverembed);
+
+}
+
+module.exports.help = {
+name: "server"
+}
