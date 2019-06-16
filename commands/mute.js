d39f067c23995022d57006d7be2bc42b4643518d
Majd Findakly <41499505+Majdziad@users.noreply.github.com>
2019-06-16 11:22:38 -0700

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const ms = require("ms");
-const superagent = require("superagent");
 const colors = require("../colors.json");
 
 module.exports.run = async (bot, message, args) => {
